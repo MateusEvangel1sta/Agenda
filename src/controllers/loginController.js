aea2e983e2c14b600950f49a34672744844c9b33
Mateus Evangelista <enzoteuprogrammer@gmail.com>
2024-10-24 20:26:25 -0300

    Completion of User Login
    We validate the user's login, save the data in cache, create the function for the user to log out of the account, among other things.

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,7 +1,9 @@
 const Login = require("../models/LoginModel");
 
 exports.index = (req, res) => {
-  res.render("login");
+  if (req.session.user) return res.render("login-logado");
+
+  return res.render("login");
 };
 
 exports.register = async (req, res) => {
@@ -15,7 +17,7 @@ exports.register = async (req, res) => {
       req.session.save(() => {
         return res.redirect("/login/index");
       });
-      
+
       return;
     }
 
@@ -29,4 +31,35 @@ exports.register = async (req, res) => {
     console.log(e);
     return res.render("404");
   }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const login = new Login(req.body);
+    await login.login();
+
+    if (login.errors.length > 0) {
+      req.flash("errors", login.errors);
+
+      req.session.save(() => {
+        return res.redirect("/login/index");
+      });
+
+      return;
+    }
+
+    req.flash("success", "Você entrou no sistema.");
+    req.session.user = login.user;
+    req.session.save(() => {
+      return res.redirect("/login/index");
+    });
+  } catch (e) {
+    console.log(e);
+    return res.render("404");
+  }
+};
+
+exports.logout = (req, res) => {
+  req.session.destroy();
+  res.redirect("/");
 };
--- src/models/LoginModel.js
+++ src/models/LoginModel.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose');
-const validator = require('validator');
-const bcrypt = require('bcryptjs'); 
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const LoginSchema = new mongoose.Schema({
   email: { type: String, required: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
 });
 
-const LoginModel = mongoose.model('Login', LoginSchema);
+const LoginModel = mongoose.model("Login", LoginSchema);
 
 class Login {
   constructor(body) {
@@ -16,9 +16,27 @@ class Login {
     this.user = null;
   }
 
+  async login() {
+    this.valid();
+    if (this.errors.length > 0) return;
+
+    this.user = await LoginModel.findOne({ email: this.body.email });
+
+    if (!this.user) {
+      this.errors.push('Usuário não existe.');
+      return
+    }
+
+    if (!bcrypt.compareSync(this.body.password, this.user.password)) {
+      this.errors.push('Senha inválida.');
+      this.user = null;
+      return
+    };
+
+  }
+
   async register() {
     this.valid();
-
     if (this.errors.length > 0) return;
 
     await this.userExists();
@@ -27,39 +45,35 @@ class Login {
 
     const salt = bcrypt.genSaltSync();
     this.body.password = bcrypt.hashSync(this.body.password, salt);
-    
-    try {
-      this.user = await LoginModel.create(this.body);
-    } catch(e) {
-      console.log(e);
-    }
+
+    this.user = await LoginModel.create(this.body);
   }
 
   async userExists() {
-    const user = await LoginModel.findOne({ email: this.body.email });
-
-    if (user) this.errors.push('Usuário já existe.');
-  };
-
+    this.user = await LoginModel.findOne({ email: this.body.email });
+    if (this.user) this.errors.push("Usuário já existe.");
+  }
 
   valid() {
     this.cleanUp();
 
-    if (!validator.isEmail(this.body.email)) this.errors.push('E-mail inválido');
-    if (this.body.password.length < 3 || this.body.password.length >= 50) this.errors.push('A senha precisa ter entre 3 e 50 caracteres.');
+    if (!validator.isEmail(this.body.email))
+      this.errors.push("E-mail inválido");
+    if (this.body.password.length < 3 || this.body.password.length >= 50)
+      this.errors.push("A senha precisa ter entre 3 e 50 caracteres.");
   }
 
   cleanUp() {
     for (let key in this.body) {
-      if (typeof this.body[key] !== 'string') {
-        this.body[key] = '';
-      };
+      if (typeof this.body[key] !== "string") {
+        this.body[key] = "";
+      }
     }
 
     this.body = {
       email: this.body.email,
-      password: this.body.password
-    }
+      password: this.body.password,
+    };
   }
 }
 
