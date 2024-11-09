c4e6a3b3132a0d09fac5917d757aa8610bf913b9
Mateus Evangelista <enzoteuprogrammer@gmail.com>
2024-11-09 09:09:29 -0300

    :sparkles: FEAT | Editing Contacts
    I made the logic responsible for editing the contacts in our database.

--- src/models/ContactModel.js
+++ src/models/ContactModel.js
@@ -55,4 +55,12 @@ Contact.prototype.cleanUp = function() {
   };
 };
 
+Contact.prototype.edit = async function(id) {
+  if (typeof id !== 'string') return;
+  this.valid();
+
+  if (this.errors.length > 0) return;
+  this.contact = await ContactModel.findByIdAndUpdate(id, this.body, { new: true });
+};
+
 module.exports = Contact;
